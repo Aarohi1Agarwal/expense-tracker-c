532d2a8314617bf469d79cb4e6c6b46b9092d4b2
Aarohi1Agarwal <aarohiagarwal987@gmail.com>
2026-06-22 00:49:23 +0530

    Created main menu

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    printf("Expense Tracker\n");
+
+    int choice;
+
+    while(1) {
+
+        printf("\n===== EXPENSE TRACKER =====\n");
+
+        printf("1. Add Expense\n");
+        printf("2. View Expenses\n");
+        printf("3. Search Expense\n");
+        printf("4. Exit\n");
+
+        printf("\nEnter choice: ");
+        scanf("%d",&choice);
+
+        switch(choice) {
+
+            case 1:
+                printf("\nAdd Expense selected\n");
+                break;
+
+            case 2:
+                printf("\nView Expenses selected\n");
+                break;
+
+            case 3:
+                printf("\nSearch Expense selected\n");
+                break;
+
+            case 4:
+                printf("\nExiting...\n");
+                return 0;
+
+            default:
+                printf("\nInvalid choice\n");
+        }
+    }
+
     return 0;
 }
